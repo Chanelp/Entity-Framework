3f0bf1d7dfa89f2e3c7738a7ac17a9244b7d066c
Chanelp <mariannissanchez27@gmail.com>
2023-01-22 12:17:39 -0400

    Utilizando base de datos en memoria

--- Program.cs
+++ Program.cs
@@ -1,6 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using proyectoef;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<TareasContext>(p => p.UseInMemoryDatabase("TareasDb"));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/dbconexion", async ([FromServices] TareasContext dbContext) => {
+    dbContext.Database.EnsureCreated();
+    return Results.Ok("Base de datos en memoria creada: " + dbContext.Database.IsInMemory());
+});
 
 app.Run();
