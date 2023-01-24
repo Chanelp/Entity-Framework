ba7b893e161f2db708ca8233c736b0b93b87031a
Chanelp <mariannissanchez27@gmail.com>
2023-01-24 14:36:51 -0400

    Conectado a base de datos SQL server desde el programa

--- Program.cs
+++ Program.cs
@@ -3,15 +3,20 @@ using Microsoft.EntityFrameworkCore;
 using proyectoef;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddDbContext<TareasContext>(p => p.UseInMemoryDatabase("TareasDb"));
+
+// Configuraciòn para conectar a bd en memoria
+// builder.Services.AddDbContext<TareasContext>(p => p.UseInMemoryDatabase("TareasDb"));
+
+// Configuraciòn para conectar a bd sql server
+builder.Services.AddSqlServer<TareasContext>("Data Source=AD-ASTRA;initial Catalog=TareasDb;user id=sa;password=micuentabd27;");
 
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "Hello World desde sql server!");
 
 app.MapGet("/dbconexion", async ([FromServices] TareasContext dbContext) => {
     dbContext.Database.EnsureCreated();
-    return Results.Ok("Base de datos en memoria creada: " + dbContext.Database.IsInMemory());
+    return Results.Ok("Base de datos sql server: " + dbContext.Database.IsInMemory());
 });
 
 app.Run();
