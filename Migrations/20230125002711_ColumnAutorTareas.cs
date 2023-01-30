5869197eb77d088b5fa28bce52f5b44085c9b1fd
Chanelp <mariannissanchez27@gmail.com>
2023-01-30 15:37:12 -0400

    Gitignore

---
+++ Migrations/20230125002711_ColumnAutorTareas.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace proyectoef.Migrations
+{
+    public partial class ColumnAutorTareas : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Autor",
+                table: "Tarea",
+                type: "nvarchar(150)",
+                maxLength: 150,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Autor",
+                table: "Tarea");
+        }
+    }
+}
---
+++ Migrations/20230125012454_InitialData.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace proyectoef.Migrations
+{
+    public partial class InitialData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Descripcion",
+                table: "Tarea",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Autor",
+                table: "Tarea",
+                type: "nvarchar(150)",
+                maxLength: 150,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(150)",
+                oldMaxLength: 150);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Descripcion",
+                table: "Categoria",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(200)",
+                oldMaxLength: 200);
+
+            migrationBuilder.InsertData(
+                table: "Categoria",
+                columns: new[] { "CategoriaId", "Descripcion", "Nombre", "Peso" },
+                values: new object[] { new Guid("3a82ce26-8edd-4c72-9b05-05872b781102"), null, "Actividades persoanles", 50 });
+
+            migrationBuilder.InsertData(
+                table: "Categoria",
+                columns: new[] { "CategoriaId", "Descripcion", "Nombre", "Peso" },
+                values: new object[] { new Guid("3a82ce26-8edd-4c72-9b05-05872b781198"), null, "Actividades pendientes", 20 });
+
+            migrationBuilder.InsertData(
+                table: "Tarea",
+                columns: new[] { "TareaId", "Autor", "CategoriaId", "Descripcion", "FechaCreacion", "PrioridadTarea", "Titulo" },
+                values: new object[] { new Guid("2bf073d5-fb9b-4081-b602-803678782d7c"), null, new Guid("3a82ce26-8edd-4c72-9b05-05872b781198"), null, new DateTime(2023, 1, 24, 21, 24, 53, 790, DateTimeKind.Local).AddTicks(1253), "Media", "Pago de servicios publicos" });
+
+            migrationBuilder.InsertData(
+                table: "Tarea",
+                columns: new[] { "TareaId", "Autor", "CategoriaId", "Descripcion", "FechaCreacion", "PrioridadTarea", "Titulo" },
+                values: new object[] { new Guid("f0de7f3b-50cf-4245-857e-7f13f13012e8"), null, new Guid("3a82ce26-8edd-4c72-9b05-05872b781102"), null, new DateTime(2023, 1, 24, 21, 24, 53, 790, DateTimeKind.Local).AddTicks(1266), "Baja", "Terminar de ver serie" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Tarea",
+                keyColumn: "TareaId",
+                keyValue: new Guid("2bf073d5-fb9b-4081-b602-803678782d7c"));
+
+            migrationBuilder.DeleteData(
+                table: "Tarea",
+                keyColumn: "TareaId",
+                keyValue: new Guid("f0de7f3b-50cf-4245-857e-7f13f13012e8"));
+
+            migrationBuilder.DeleteData(
+                table: "Categoria",
+                keyColumn: "CategoriaId",
+                keyValue: new Guid("3a82ce26-8edd-4c72-9b05-05872b781102"));
+
+            migrationBuilder.DeleteData(
+                table: "Categoria",
+                keyColumn: "CategoriaId",
+                keyValue: new Guid("3a82ce26-8edd-4c72-9b05-05872b781198"));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Descripcion",
+                table: "Tarea",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Autor",
+                table: "Tarea",
+                type: "nvarchar(150)",
+                maxLength: 150,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(150)",
+                oldMaxLength: 150,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Descripcion",
+                table: "Categoria",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(200)",
+                oldMaxLength: 200,
+                oldNullable: true);
+        }
+    }
+}
